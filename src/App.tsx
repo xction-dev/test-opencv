36c7a0b769b7186bf67ada31881eb436f0f0d64b
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-05-22 14:46:07 +0900

    Feat: Video 테스트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import cv from "@techstark/opencv-js";
+
+const sampleVideoURL =
+  "https://d2yo7lrb9dagdw.cloudfront.net/hd/devsample_A_hd.mp4";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <video
+        src={sampleVideoURL}
+        onCanPlay={(e) => {
+          const capture = new cv.VideoCapture(e.target as HTMLVideoElement);
+          console.dir(capture);
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
